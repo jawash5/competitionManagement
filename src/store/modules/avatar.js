0ff389997da746cc22f786a13c515cc5ca12365f
jawash5 <857723555@qq.com>
2020-11-21 15:40:32 +0800

    头像显示修复2.0

--- src/store/modules/avatar.js
+++ src/store/modules/avatar.js
@@ -16,7 +16,7 @@ const mutations = {
 const actions = {
     setAvatarUrl({commit}, data) {
         return new Promise(resolve => {
-            const value = data.replace('-internal')
+            const value = data.replace('-internal', '')
             commit('SET_AVATAR_URL', value)
             sessionStorage.setItem('avatar', data)
             return resolve();
